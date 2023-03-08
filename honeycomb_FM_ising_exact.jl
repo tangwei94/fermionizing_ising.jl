ffc328f1485b83bb7dbc966ef6787400614a80c0
Wei Tang <tangwei@smail.nju.edu.cn>
2023-03-08 01:18:21 +0100

    honeycomb lattice exact solution

--- honeycomb_FM_ising_exact.jl
+++ honeycomb_FM_ising_exact.jl
@@ -1,10 +1,11 @@
 using LinearAlgebra
 using CairoMakie 
 using Interpolations
+using Polynomials
 
 # honeycomb lattice. spatial lattice basis
 
-function free_energy_density_honeycomb(N::Int, β::Real, α::Real=5)
+function f_density_honeycomb_α(N::Int, β::Real, α::Real=5)
 
     β1 = -0.5 * log(tanh(β)) # β^\ast
 
@@ -36,40 +37,48 @@ function free_energy_density_honeycomb(N::Int, β::Real, α::Real=5)
     Λ, _ = eigen(Hermitian(RT))
 
     ϵs = log.(Λ[N+1:end])
-    #ϵ1s = -log.(Λ[1:N]) # ϵs is more stable and more accurate
+    #ϵ1s = -log.(Λ[1:N]) # the positive entries are more stable and more accurate
 
     f = -sum(ϵs) / (4*N*β) - log(2*sinh(2*β)) / (4*β) + log(cosh(α)) / (2*β)
-    #f1 = -sum(ϵ1s) / (4*N*β) - log(2*sinh(2*β)) / (4*β) + log(cosh(α)) / (2*β)
-    #@show log.(Λ)
     return f
 end
 
-β = 1
-βc = asinh(sqrt(3)) / 2 
-
-αs = 3:0.2:7
-fs = free_energy_density_honeycomb.(12, βc-0.1, αs)
-f1s = free_energy_density_honeycomb.(12, βc+0.1, αs)
-
-fig = Figure(backgroundcolor = :white, fontsize=18, resolution= (600, 400))
-ax1 = Axis(fig[1, 1],)
-sc1_orig = scatter!(ax1, 1 .- tanh.(αs), fs, marker=:dot, markersize=10)
-sc1_orig = scatter!(ax1, 1 .- tanh.(αs), f1s, marker=:dot, markersize=10)
-@show fig
-
-for N in 8:8:100
-    @show free_energy_density_honeycomb(N, βc-0.1)
+function f_density_honeycomb(N::Int, β::Real)
+    αs = 3:0.2:5
+    fs = free_energy_density_honeycomb.(N, β, αs)
+    fit_linf = Polynomials.fit(1 .- tanh.(αs), fs, 1)
+    return fit_linf[0]
 end
 
-N = 50
+βc = asinh(sqrt(3)) / 2 
 
-βs = Vector(βc .+ 0.01 .* (-20:20))
-fs = free_energy_density_honeycomb.(N, βs, 7)
+# scaling vs α
+αs = 3:0.2:5
+fs = free_energy_density_honeycomb.(12, βc, αs)
+fig_α = Figure(backgroundcolor = :white, fontsize=18, resolution= (600, 400))
+ax1 = Axis(fig_α[1, 1],)
+sc1 = scatter!(ax1, 1 .- tanh.(αs), fs, marker=:dot, markersize=10)
+@show fig_α
 
-itp = interpolate((βs,), fs, Gridded(Linear()));
-Es = only.(Interpolations.gradient.(Ref(itp), βs)) .* βs
-itp2 = interpolate((βs,), Es, Gridded(Linear()));
-Cvs = only.(Interpolations.gradient.(Ref(itp2), βs)) .* (-βs .^ 2)
+# scaling vs N
+Ns = 8:8:120
+f1s = free_energy_density_honeycomb.(Ns, βc)
+f2s = free_energy_density_honeycomb.(Ns, βc-0.1)
+f3s = free_energy_density_honeycomb.(Ns, βc+0.1)
+fig_N = Figure(backgroundcolor = :white, fontsize=18, resolution= (600, 400))
+ax1 = Axis(fig_N[1, 1], yscale=log10)
+sc1 = lines!(ax1, Ns, f1s .- f1s[end] .+ 1e-3, marker=:dot, markersize=10, label=L"\beta=\beta_c")
+sc2 = lines!(ax1, Ns, f2s .- f2s[end] .+ 1e-3, marker=:dot, markersize=10, label=L"\beta=\beta_c-0.1")
+sc3 = lines!(ax1, Ns, f3s .- f3s[end] .+ 1e-3, marker=:dot, markersize=10, label=L"\beta=\beta_c+0.1")
+axislegend(ax1, position=:rt)
+@show fig_N
+
+N = 100
+Δβ = 0.0025
+βs = Vector(βc .+ Δβ .* (-20:20))
+fs = free_energy_density_honeycomb.(N, βs)
+Es = (fs[3:end] - fs[1:end-2]) / (2*Δβ) .* βs[2:end-1]
+Cvs = -(Es[3:end] - Es[1:end-2]) / (2*Δβ) .* (βs[3:end-2] .^ 2)
 
 fig = Figure(backgroundcolor = :white, fontsize=18, resolution= (600, 800))
 
@@ -77,22 +86,29 @@ ax1 = Axis(fig[1, 1],
         xlabel = L"\beta",
         ylabel = L"f", 
         )
-
-sc1_orig = scatter!(ax1, βs, fs, marker=:circle, markersize=15)
-@show fig
-
 ax2 = Axis(fig[2, 1], 
         xlabel = L"\beta",
         ylabel = L"E", 
         )
-
-sc2_orig = scatter!(ax2, βs, Es, marker=:o, markersize=15)
-@show fig
-
 ax3 = Axis(fig[3, 1], 
         xlabel = L"\beta",
         ylabel = L"C_V", 
         )
+#itp = interpolate((βs,), fs, Gridded(Linear()));
+#Es = only.(Interpolations.gradient.(Ref(itp), βs)) .* βs
+#itp2 = interpolate((βs,), Es, Gridded(Linear()));
+#Cvs = only.(Interpolations.gradient.(Ref(itp2), βs)) .* (-βs .^ 2)
 
-sc3_orig = scatter!(ax3, βs, Cvs, marker=:o, markersize=15)
+sc1 = scatter!(ax1, βs, fs, markersize=10, label="N=$(N)")
+sc2 = scatter!(ax2, βs[2:end-1], Es, markersize=10, label="N=$(N)")
+sc3 = scatter!(ax3, βs[3:end-2], Cvs, markersize=10, label="N=$(N)")
+
+sc2 = lines!(ax2, fill(βc, 2), [0.1, 0.9], color=:grey)
+sc2 = lines!(ax3, fill(βc, 2), [0.5, 5], color=:grey)
+
+axislegend(ax1)
+axislegend(ax2)
+axislegend(ax3)
 @show fig
+
+save("honeycomb_lattice_exact.pdf", fig)
--- square_FM_ising_exact.jl
+++ square_FM_ising_exact.jl
@@ -1,4 +1,4 @@
-function free_energy_density_square(N::Int, β::Real)
+function f_density_square(N::Int, β::Real)
 
     β1 = -0.5 * log(tanh(β)) # β^\ast
 
@@ -28,15 +28,17 @@ end
 βc = asinh(1) / 2
 fs = free_energy_density_latt.(8:8:98, βc)
 
-N = 50 
+N = 100 
 
-βs = Vector(βc .+ 0.01 .* (-20:20))
-fs = free_energy_density_latt.(N, βs)
-
-itp = interpolate((βs,), fs, Gridded(Linear()));
-Es = only.(Interpolations.gradient.(Ref(itp), βs)) .* βs
-itp2 = interpolate((βs,), Es, Gridded(Linear()));
-Cvs = only.(Interpolations.gradient.(Ref(itp2), βs)) .* (-βs .^ 2)
+Δβ = 0.001
+βs = Vector(βc .+ Δβ .* (-20:20))
+fs = f_density_square.(N, βs)
+Es = (fs[3:end] - fs[1:end-2]) / (2*Δβ) .* βs[2:end-1]
+Cvs = -(Es[3:end] - Es[1:end-2]) / (2*Δβ) .* (βs[3:end-2] .^ 2)
+#itp = interpolate((βs,), fs, Gridded(Linear()));
+#Es = only.(Interpolations.gradient.(Ref(itp), βs)) .* βs
+#itp2 = interpolate((βs,), Es, Gridded(Linear()));
+#Cvs = only.(Interpolations.gradient.(Ref(itp2), βs)) .* (-βs .^ 2)
 
 fig = Figure(backgroundcolor = :white, fontsize=18, resolution= (600, 800))
 
@@ -45,7 +47,7 @@ ax1 = Axis(fig[1, 1],
         ylabel = L"f", 
         )
 
-sc1_orig = scatter!(ax1, βs, fs, marker=:o, markersize=15)
+sc1_orig = scatter!(ax1, βs, fs, marker=:o, markersize=10)
 @show fig
 
 ax2 = Axis(fig[2, 1], 
@@ -53,7 +55,7 @@ ax2 = Axis(fig[2, 1],
         ylabel = L"E", 
         )
 
-sc2_orig = scatter!(ax2, βs, Es, marker=:o, markersize=15)
+sc2_orig = scatter!(ax2, βs[2:end-1], Es, marker=:o, markersize=10)
 @show fig
 
 ax3 = Axis(fig[3, 1], 
@@ -61,5 +63,7 @@ ax3 = Axis(fig[3, 1],
         ylabel = L"C_V", 
         )
 
-sc3_orig = scatter!(ax3, βs, Cvs, marker=:o, markersize=15)
+sc3_orig = scatter!(ax3, βs[3:end-2], Cvs, marker=:o, markersize=10)
 @show fig
+
+save("square_lattice.pdf", fig)
