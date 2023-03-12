02c9334aff09ed843ba4730b03f414db6cb56f9f
Wei Tang <wei.tang.nju@gmail.com>
2023-03-12 20:37:18 +0100

    update

--- VUMPS/triangular_lattice_VUMPS.jl
+++ VUMPS/triangular_lattice_VUMPS.jl
@@ -33,7 +33,7 @@ T2 = permute(sqrt(S) * V, (1, 2), (3, 4))
 # test at finite size
 fN1s = Float64[]
 fN2s = Float64[]
-for N in [2, 3, 4, 5, 6, 7, 8]
+for N in [2, 3, 4, 5, 6]
     𝕋N = reduce(vcat, fill([M1, M2], N))
     𝕋N_mat = convert_to_mat(𝕋N)
     Λ, _ = eigen(𝕋N_mat)
@@ -44,21 +44,32 @@ for N in [2, 3, 4, 5, 6, 7, 8]
     println("$(N), $(fN), $(fN2), $(fN - fN2)")
 end
 
-#check 
-# exact solution. extrapolated from finite size results
+𝕋N = reduce(vcat, fill([M1, M2], 4))
+𝕋N_mat = convert_to_mat(𝕋N)
+Λ, _ = eigen(𝕋N_mat)
+Λs =  Λ.data |> diag
+Λm = norm(last(Λs))
+
+fig = Figure(backgroundcolor = :white, fontsize=18, resolution= (400, 400))
+ax1 = Axis(fig[1, 1])
+xlims!(ax1, (-Λm, Λm))
+ylims!(ax1, (-Λm, Λm))
+scatt_N = scatter!(ax1, real.(Λs), imag.(Λs), marker=:circle, markersize=10)
+@show fig
+
+#check. if f_density_triangular has errors 
 Ns = 40:8:200
-f_imaging(x) = -0.80 * x^(-2) + f0 + 1e-7 *rand() * x
+f_imaging(x) = -0.80 * x^(-2) + 1 + 1e-7 *rand() * x
 
 @. f_fit(x, p) = p[1]*x^p[2] + p[3]
-p0 = [1.0, -1.0, last(fNs) + 1e-6]
+p0 = [1.0, -1.0, 1.1]
 fit_outcome_aaa = curve_fit(f_fit, Ns, f_imaging.(Ns), p0)
-pf_aaa = coef(fit_outcome)
+pf_aaa = coef(fit_outcome_aaa)
 
 fig = Figure(backgroundcolor = :white, fontsize=18, resolution= (600, 400))
 ax1 = Axis(fig[1, 1], xlabel=L"1/\chi \text{ or } 10 / N", ylabel=L"|f-f_{\mathrm{exact}}|", yscale=log10, xscale=log10)
 scatt_N = scatter!(ax1, 10 ./ Ns, abs.(f_imaging.(Ns) .- pf_aaa[3]) .+ 1e-14, marker=:circle, markersize=10)
 @show fig
-
 
 # hermicity
 𝕋N = reduce(vcat, fill([M1, M2], 4));
@@ -69,7 +80,7 @@ scatt_N = scatter!(ax1, 10 ./ Ns, abs.(f_imaging.(Ns) .- pf_aaa[3]) .+ 1e-14, ma
 @show norm(𝕋N_mat * 𝕋N_mat' - 𝕋N_mat' * 𝕋N_mat)
 
 # VUMPS
-χs = 4:4:20
+χs = 4:4:40
 fχs = Float64[]
 for χ in χs
     A = TensorMap(rand, ComplexF64, ℂ^χ*ℂ^2, ℂ^χ);
@@ -92,9 +103,11 @@ f0 = pf[3]
 fit_outcome2 = curve_fit(f_fit, χs[3:end], fχs[3:end], p0)
 f02 = coef(fit_outcome2)[3]
 
+@show f02 - f0
+
 fig = Figure(backgroundcolor = :white, fontsize=18, resolution= (600, 400))
 ax1 = Axis(fig[1, 1], xlabel=L"1/\chi \text{ or } 10 / N", ylabel=L"|f-f_{\mathrm{exact}}|", yscale=log10, xscale=log10)
-scatt_χ = scatter!(ax1, 1 ./ χs, abs.(fχs .- f0), marker=:circle, markersize=10)
+scatt_χ = scatter!(ax1, 2 ./ χs, abs.(fχs .- f0), marker=:circle, markersize=10)
 scatt_N = scatter!(ax1, 10 ./ Ns, abs.(fNs .- f0), marker=:circle, markersize=10)
 @show fig
 
